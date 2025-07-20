6b499c99cc376fac9b15df34b975677592605733
Varun2976 <vsnair2976@gmail.com>
2025-07-20 15:45:57 +0530

    push-front

--- Linked_List/Insertion/After_front.cpp
+++ Linked_List/Insertion/After_front.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+//************************************* PUSH - FRONT ************************************
+
 
 class Node{
     public:
@@ -14,9 +16,38 @@ class Node{
 class List{
     Node *head;
     Node *tail;
+    public :
+    List() {
+        head = tail = NULL;
+        }
+    void push_front(int val){
+        Node *newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode; // Determines that my linked list is empty since the last and first both would be NULL
+        }
+        else{
+            newNode -> next = head;
+            head = newNode;
+        }
+    
+    }
+    void printLL(){
+        Node *temp = head;
+        while (temp != NULL){
+            cout << temp -> data << " ";
+            temp = temp ->next;
+        }
+    }
 };
 int main()
 {
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
+
+    ll.printLL();
     return 0;
 }
 
