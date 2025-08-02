d0c3ab1d4359727c9c2c6d8f33ede3a1a7781792
Varun2976 <vsnair2976@gmail.com>
2025-08-02 08:47:02 +0530

    Binary Search

---
+++ Arrays/Binary_Search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// ----------------------- BINARY SEARCH ----------------------------
+// Time Complexity = O(logn)
+// Can be applied only in sorted arrays , be it ascending or descending.
+
+int Binary_search(vector<int> arr , int tar)
+{
+    int st = 0; 
+    int end = arr.size() - 1;
+
+    while(st <= end){
+        int mid = (st + end) / 2; 
+
+        if(tar > arr[mid]){
+            st = mid + 1; 
+        }
+        else if(tar < arr[mid]){
+            end = mid - 1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector <int> arr = {0,1,2,3,4,5,6,7,8};
+    int target = 6;
+
+    cout << Binary_search(arr,target) << endl;
+    return 0;
+}
