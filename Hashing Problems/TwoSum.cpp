9d51aff6b34e3ff15fd0fe4224593329d88a8cd3
Varun2976 <vsnair2976@gmail.com>
2025-08-19 00:56:10 +0530

    Twosum

--- Hashing Problems/TwoSum.cpp
+++ Hashing Problems/TwoSum.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+using namespace std;
 
-int main(){
+int BruteForce(int arr[] , int target){
+    int first, second,sum;
+    for(int i=0 ; i < 5; i++){
+        first = arr[i];
+        for(int j = i+1 ; j < 5 ; j++){
+            second = arr[j];
+            sum = first + second;
+            if(sum == target){
+                cout << "Indices: " << i << ", " << j << endl;
+                cout << "Numbers: " << arr[i] << ", " << arr[j] << endl;
+                return 0;
+            }
+
+        }
+    }
+    return -1;
     
+}
+int main(){
+    int arr[5] = {5,2,11,7,15};
+    int target = 9;
+
+    BruteForce(arr,target);
     
     
     return 0;
