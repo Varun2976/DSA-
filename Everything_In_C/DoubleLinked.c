ba3366f0ab3de9bed4e98fe143b02b1adc730772
Varun2976 <vsnair2976@gmail.com>
2025-09-10 21:41:41 +0530

    Insert at end in doubly linked list

---
+++ Everything_In_C/DoubleLinked.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next, *prev;
+}*new_record, *first,*last,*temp;
+void create()
+{
+    first = NULL;
+    last = NULL;
+}
+void insert_at_end(){
+    new_record = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the value you wanna insert : ");
+    scanf("%d" , &new_record -> data);
+
+    if(first == NULL){
+        new_record -> next = NULL;
+        new_record -> prev = NULL;
+        first = new_record;
+        last = new_record;
+    }
+    else{
+        new_record -> next = NULL;
+        new_record -> prev = last;
+        last -> next = new_record;
+        last = new_record;
+
+    }
+
+}
+void display(){
+    if(first == NULL){
+        printf("The list is empty.");
+    }
+    temp = first;
+    while(first != NULL){
+        printf("%d" , temp -> data);
+        temp = temp -> next;
+    }
+}
+int main(){
+    insert_at_end();
+    insert_at_end();
+    insert_at_end();
+    insert_at_end();
+    insert_at_end();
+    display();
+
+    
+    return 0;
+}
