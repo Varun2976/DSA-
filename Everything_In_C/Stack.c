25c8d00fb63de0cfe593106ada6d6b2bf9bbaf0f
Varun2976 <vsnair2976@gmail.com>
2025-09-16 02:08:33 +0530

    Stack push

---
+++ Everything_In_C/Stack.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct stack{
+    int data;
+    struct stack *next;
+}*new_record,*temp;
+struct stack *top = NULL;
+void create(){
+    top = NULL;
+
+}
+int isEmpty(){
+    return(top == NULL);
+}
+void push(){
+    new_record = (struct stack *)malloc(sizeof(struct stack));
+    printf("Enter the value you wanna insert at the top : ");
+    scanf("%d" , &new_record -> data);
+    if(new_record == NULL){
+        printf("The stack is full.");
+    }
+    new_record -> next = top;
+    top = new_record;
+}
+void display(){
+    if(isEmpty()){
+        printf("Stack is empty");
+    }
+    struct stack *temp = top;
+    while(temp != NULL){
+        printf("%d" , temp -> data);
+        temp = temp -> next;
+    }
+
+    
+}
+int main(){
+
+    push();
+    push();
+    push();
+    push();
+    display();
+    return 0;
+}
