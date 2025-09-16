2831c01d0649d2cbc5d200e91679959d368e0bcc
Varun2976 <vsnair2976@gmail.com>
2025-09-16 07:47:27 +0530

    queue

--- Everything_In_C/Queues/NormalQueue.c
+++ Everything_In_C/Queues/NormalQueue.c
@@ -4,7 +4,7 @@
 struct queue{
     int data;
     struct queue *next;
-}*front , *rear,*new_record;
+}*front , *rear,*new_record,*temp;
 
 void create(){
     struct queue *front = NULL;
@@ -12,6 +12,23 @@ void create(){
 }
 void enqueue(int val){
     new_record = (struct queue *)malloc(sizeof(struct queue));
+    
+    if(rear == NULL){
+        front = rear = NULL;
+    }
+    else{
+        rear -> next = new_record;
+        rear = new_record;
+    }
+}
+void dequeue(){
+    if(front == NULL){
+        printf("The stack is empty.");
+    }
+    temp = front;
+    front = front -> next;
+    free(temp);
+
     
 }
 
