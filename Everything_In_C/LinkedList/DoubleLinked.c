ffe1d625097bd2313d82125a84e2d0f37d30a2a0
Varun2976 <vsnair2976@gmail.com>
2025-10-23 14:37:17 +0530

    Insert at middle

--- Everything_In_C/LinkedList/DoubleLinked.c
+++ Everything_In_C/LinkedList/DoubleLinked.c
@@ -12,7 +12,7 @@ void create()
 }
 void insert_at_end(){
     new_record = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the value you wanna insert : ");
+    printf("\nEnter the value you wanna insert : ");
     scanf("%d" , &new_record -> data);
 
     if(first == NULL){
@@ -32,7 +32,7 @@ void insert_at_end(){
 }
 void insert_at_front(){
     new_record = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the value you wanna insert : ");
+    printf("\nEnter the value you wanna insert : ");
     scanf("%d" , &new_record -> data);
     new_record -> prev = NULL;
 
@@ -45,6 +45,33 @@ void insert_at_front(){
         first -> prev = new_record;
         first = new_record;
     }
+
+}
+void insert_at_middle(int pos){
+    if(pos <= 1){
+        insert_at_front();
+        return;
+    }
+    temp = first;
+    int count = 1;
+
+    while(temp!=NULL && count < pos -1){
+        temp = temp -> next;
+        count++;
+    }
+    if(temp == NULL || temp == last){
+        insert_at_end();
+        return;
+    }
+    new_record=(struct node *)malloc(sizeof(struct node));
+
+    printf("\nEnter a value to insert at position : ");
+    scanf("%d" , &new_record -> data);
+
+    new_record -> next = temp -> next;
+    new_record->prev = temp;
+    temp -> next -> prev = new_record;
+    temp -> next = new_record;
 
 }
 void delete_at_front(){
@@ -72,8 +99,8 @@ void display(){
         printf("The list is empty.");
     }
     temp = first;
-    while(first != NULL){
-        printf("%d" , temp -> data);
+    while(temp != NULL){
+        printf("%d ->" , temp -> data);
         temp = temp -> next;
     }
 }
@@ -82,8 +109,9 @@ int main(){
     insert_at_end();
     insert_at_end();
     insert_at_end();
-    insert_at_end();
-    delete_at_end();
+    
+    display();
+    insert_at_middle(3);
     display();
 
     
