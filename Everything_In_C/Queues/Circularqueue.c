98dce66c9f7f5bfe9d5a4969bb2a347a0348df96
Varun2976 <vsnair2976@gmail.com>
2025-11-10 15:58:20 +0530

    .

--- Everything_In_C/Queues/Circularqueue.c
+++ Everything_In_C/Queues/Circularqueue.c
@@ -1,22 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 struct Node{
     int data;
     struct Node *next;
 }*first,*last,*temp;
 
 void enqueue(int value){
+    temp = (struct Node  *)malloc(sizeof(struct Node));
     temp -> data = value;
-    temp -> next = NULL;
+    
 
     if(first == NULL){
-        first = last = NULL;
-        last ->
-        
-
+        first = last = temp;
+        last -> next = first;
+    }
+    else{
+        last -> next = temp;
+        last = temp;
+        last -> next = first;
     }
 
 }
+void dequeue(){
+    if(first == NULL){
+        printf("Queue is empty");
+        return;
+    }
+    if(first == last){
+        free(first);
+        first = last = NULL;
+    }
+    else{
+        struct Node *del = first;
+        first = first -> next;
+        last -> next = first;
+        free(del);
+    }
+}
+void display(){
+    if(first == NULL){
+        printf("Queue is empty");
+        return;
+    }
+    struct Node *ptr = first;
+    do{
+        printf("%d -> " , ptr -> data);
+        ptr = ptr -> next;
+    }while(ptr != first);
+}
 int main(){
+    enqueue(12);
+    enqueue(24);
+    enqueue(45);
+    enqueue(2);
+    enqueue(22);
+    display();
+    printf("\n");
+    dequeue();
+    display();
+    
     
     return 0;
 }
