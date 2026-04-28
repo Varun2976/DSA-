d419344bb5ef55c9f8fd0de5806725b08d37be87
Varun2976 <vsnair2976@gmail.com>
2026-04-29 02:47:37 +0530

    Stock span problem

--- Stacks/StockSpan.cpp
+++ Stacks/StockSpan.cpp
@@ -4,6 +4,24 @@ using namespace std;
 //span = maximum number of days from today price <= current_price
 
 int main(){
+    vector<int>price ={100,80,60,70,60,75,85};
+    vector<int>ans(price.size(),0);
+    stack <int> s;
 
+    for(int i = 0 ; i < price.size();i++){
+        while(s.size() > 0 && price[s.top()] <= price[i]){
+            s.pop();
+        }
+        if(s.empty()){
+            ans[i] = i + 1;
+        }else{
+            ans[i] = i - s.top();
+        }
+        s.push(i);
+    }
+    for(int val : ans){
+        cout << val << " "
+    }
+    cout << endl;
     return 0;
 }
