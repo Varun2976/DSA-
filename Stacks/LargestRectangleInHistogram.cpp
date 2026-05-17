9bc69aea460fbbc6f777b702b9d0149c58cf06d9
Varun2976 <vsnair2976@gmail.com>
2026-05-17 10:08:04 +0530

    Added Brute force approach of Largest Histogram Rectangle

--- Stacks/LargestRectangleInHistogram.cpp
+++ Stacks/LargestRectangleInHistogram.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int Brute(vector<int> &heights){
+    int n = heights.size();
+    int maxi = 0;
+    height = heights[i]
+    for(int i = 0 ; i < n ; i++){
+        int left = i;
+        int right = i;
 
+        while(left-1 >= 0 && heights[i-1] >= height){
+            left--;
+        }
+        while(right+1 < n && heights[i+1] >= height){
+            right++;
+        }
+
+        int width = right - left + 1;
+        int area = width * height;
+
+        maxi = max(maxi,area);
+    }
+    return maxi;
+}
 
 int main(){
-    
+    vector<int>heights = {2,1,5,6,2,3}
     return 0;
 }
