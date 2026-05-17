6e82d6ae29cf19c3cc92d42b4831dcba932de66a
Varun2976 <vsnair2976@gmail.com>
2026-05-17 10:56:37 +0530

    Added the optimal solution

--- Stacks/LargestRectangleInHistogram.cpp
+++ Stacks/LargestRectangleInHistogram.cpp
@@ -24,6 +24,45 @@ int Brute(vector<int> &heights){
     return maxi;
 }
 
+int optimal(vector<int>&heights){
+    int n = heights.size();
+    stack<int> s;
+    vector<int> left(n,0);
+    vector<int> right(n,0);
+
+    //Right Smaller Element
+    for(int i = n -1 ; i >= 0;i--){
+        while(s.size() > 0 && heights[s.top()] >= heights[i]){
+            s.pop();
+        }
+
+        right[i] = s.empty() ? -1 : s.top();
+        s.push(i);
+    }
+
+    while(!s.empty()){
+        s.pop();
+    }
+
+    //Left Smaller Element
+    for(int i = 0 ; i < n;i++){
+        while(s.size() > 0 && heights[s.top()] >= heights[i]){
+            s.pop();
+        }
+
+        left[i] = s.empty() ? -1 : s.top();
+        s.push(i);
+    }
+
+    for(int i = 0 ; i < n ;i++){
+        int width = right[i] - left[i] - 1;
+        int area = heights[i] * width;
+
+        ans = max(ans,area);
+    }
+    return ans;
+}
+
 int main(){
     vector<int>heights = {2,1,5,6,2,3}
     return 0;
