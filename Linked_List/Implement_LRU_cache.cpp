a8b0b338092f71cbf96eb772034f3c39a6136cbe
Varun2976 <vsnair2976@gmail.com>
2026-06-03 10:26:17 +0530

    Till now

--- Linked_List/Implement_LRU_cache.cpp
+++ Linked_List/Implement_LRU_cache.cpp
@@ -1,6 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+        int val;
+        int key;
+        Node* prev;
+        Node* next;
+
+
+        Node(int k ,int v){
+            key = k;
+            val = v;
+            prev = next = NULL;
+        }
+
+        
+
+}
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+
+    unordered_map<int,Node*>m;
+    int limit;
+
+void addNode(Node* newNode){
+    Node* oldNext = head -> next;
+    head -> next = newNode;
+    oldNext -> prev = newNode;
+
+    newNode -> next = oldNext;
+    newNode -> prev = head;
+
+
+}
+void delNode(Node* oldNode){
+    Node* oldPrev = oldNode -> prev;
+    Node* oldNext = oldNode -> next;
+
+    oldPrev -> next = oldNext;
+    oldNext -> prev = oldPrev;
+}
+LRU_Cache(int capacity){
+    limit = capacity;
+    head -> next = tail;
+    tail -> prev = head;
+}
+
+
+
+int get(int key){
+
+}
+void put(int key, int value){
+    if(m[key] != m.end(){
+        Node* oldNode = m[key];
+        delNode(oldNode);
+        m.erase(key);
+    })
+}
 int main(){
-    
+
+    return 0;
 }
