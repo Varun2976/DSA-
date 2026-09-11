b3af86adde305b5ea5e97e2eb3495100b17e3e03
Varun2976 <vsnair2976@gmail.com>
2026-09-11 11:58:48 +0530

    Level order

---
+++ Trees/TopViewOfATree.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node*{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val){
+            data = val;
+            left = right = NULL;
+        }
+}
+int main(){
+    return 0;
+}
